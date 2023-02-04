b1d2ab013779a7178d7054f8770e52d920e81ca8
yajium <yajiuma720@gmail.com>
2023-02-05 00:36:17 +0900

    fix: design

--- src/pages/user/edit.tsx
+++ src/pages/user/edit.tsx
@@ -88,19 +88,19 @@ const Mypage = () => {
           <div className="mx-5 mt-4 rounded-3xl bg-white bg-opacity-20 px-8 py-4 font-dot text-white md:mx-0">
             {error && <ErrorMessage message={error} addMsg="名前は" />}
             <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-              <div className="mb-8 font-medium caret-orange-400">
+              <div className="mx-4 mb-8 font-medium caret-orange-400">
                 <InputField
                   label={f.LABEL_NAME}
                   register={register("name")}
                   placeholder={f.PLACE_NAME}
-                  className="form-input mb-3 mt-1 block w-full rounded-md text-black shadow-sm outline outline-offset-0 outline-teal-100 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  className="form-input mb-3 mt-1 block w-full rounded-md text-black outline outline-offset-0 outline-teal-100 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 ></InputField>
                 <TextareaField
                   label={f.LABEL_COMMENT}
                   register={register("comment")}
                   error={errors.comment?.message}
                   placeholder={f.PLACE_COMMENT}
-                  className="form-input mt-4 block w-full resize-none rounded-md text-black shadow-sm outline outline-offset-0 outline-teal-100 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  className="form-input mt-4 w-full resize-none rounded-md py-5 text-black outline outline-offset-0 outline-teal-100 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 md:py-8"
                 ></TextareaField>
               </div>
               <div className="text-center font-dot text-white">
@@ -108,7 +108,7 @@ const Mypage = () => {
                   <Pokemons pokemons={userInfo.pokemons} isEdit></Pokemons>
                 ) : (
                   <div className="grid grid-rows-2">
-                    <p className="mt-10 mb-20 text-base font-medium md:text-3xl">
+                    <p className="mt-10 mb-20 text-base font-medium md:text-2xl">
                       {f.NON_POKE}
                     </p>
                     <Image
@@ -124,7 +124,7 @@ const Mypage = () => {
               <div className="flex justify-center md:mt-12">
                 <Button
                   type="submit"
-                  className="mx-2 mb-10 rounded-full border-2 border-teal-300 bg-white px-3 py-2 text-center text-xl font-medium text-teal-300 opacity-95 hover:text-teal-100 md:px-6 md:py-3 md:text-2xl"
+                  className="mx-2 mb-10 rounded-full border-2 border-teal-300 bg-white px-3 py-2 text-center text-xl font-medium text-teal-300 opacity-95 hover:text-teal-100 md:text-2xl"
                 >
                   <CheckIcon className="inline-block h-8 w-8 align-top" />
                   {f.BTN_SAVE}
