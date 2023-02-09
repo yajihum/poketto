0cee4bfc578e6a89b8402e761f42a339849d548a
yajium <yajiuma720@gmail.com>
2023-02-09 23:19:43 +0900

    fix: add file extension

---
+++ src/pages/api/user/free/info.ts
@@ -0,0 +1,29 @@
+import type { NextApiResponse } from "next";
+import {
+  APIResponse,
+  NextApiRequestWithBody,
+} from "../../../../types/APIResponse";
+import methodHandler from "../../../../util/methodHandler";
+import fetchResponse from "../../../../util/fetchResponse";
+import { fetchUserInfo } from "../../../../lib/module/user";
+import errorHandler from "../../../../util/errorHandler";
+
+const handler = (
+  req: NextApiRequestWithBody,
+  res: NextApiResponse<APIResponse>
+) => {
+  const { id } = req.query;
+
+  if (typeof id !== "string") return errorHandler(res, 501);
+
+  console.log(id);
+  methodHandler(
+    {
+      GET: () => fetchResponse(fetchUserInfo(id), res),
+    },
+    req.method,
+    res
+  );
+};
+
+export default handler;
